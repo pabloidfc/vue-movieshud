cc72fb4e66c0f43763fd097b92d5137d417184fb
Pablo Rodriguez <pablordzinfo@gmail.com>
2024-02-05 18:26:41 -0300

    creating SeachInput

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <input
+      class="w-full m-0 ps-10 py-2 placeholder:text-white placeholder:font-medium placeholder:tracking-wide rounded-3xl"
+      type="search" name="search-input" id="search-input" placeholder="Buscar...">
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import MainHeader from '../components/MainHeader.vue';
+import MainHeader from '../components/layout/MainHeader.vue';
+import SearchInput from '../components/SearchInput.vue';
 </script>
 
 <template>
   <MainHeader />
+  <main class="flex flex-col items-center w-full px-5">
+    <section class="w-full">
+      <SearchInput />
+    </section>
+  </main>
 </template>
 
 <style scoped></style>
